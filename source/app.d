670b494711aaa88addbe47dc49cbdb3fdf5c7248
Forb.Jok <forbjok@gmail.com>
2018-12-01 15:28:32 +0100

    304 timeout bug

--- source/app.d
+++ source/app.d
@@ -1,12 +1,32 @@
+import std.conv : text;
 import std.stdio : writeln;
-import requests : getContent;
+import requests : Request;
 
 void main()
 {
   writeln("BEFORE");
 
-  auto content = getContent("http://httpbin.org/");
-  writeln(content);
+  auto url = "http://boards.4channel.org/g/thread/51971506";
+
+  string[string] requestHeaders;
+
+  requestHeaders["if-modified-since"] = "Fri, 07 Sep 2018 23:58:36 GMT";
+
+  // Construct request
+  auto request = Request();
+  request.verbosity = 2;
+
+  request.addHeaders(requestHeaders);
+
+  // Execute request
+  auto response = request.get(url);
+
+  // If response indicates non-success, return without processing content
+  if (response.code != 200)
+    return;
+
+  // Report progress
+  writeln("RECEIVED " ~ text(response.contentReceived, " of ", response.contentLength));
 
   writeln("AFTER");
 }
